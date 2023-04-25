c1ff086f339c9536ef5cfde69be3620bfe38a11e
mjaqueFL <61022525+mjaqueFL@users.noreply.github.com>
2023-04-25 13:41:10 +0200

    Comentado script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 'use strict'
 
+/**
+  Valida que los campos del formulario cumplan las reglas establecidas.
+  @return {boolean} True si cumple las reglas, false en caso contrario.
+**/
 function validarFormulario() {
 	var nombre = document.querySelector('input').value;
 	var raza = document.querySelector('select').value;
